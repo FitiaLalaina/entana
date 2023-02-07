a4d968789b3c4b67a55540c018b13b9c86ce051c
FitiaLalaina <fitialalainarad@gmail.com>
2023-02-07 16:07:26 +0300

    gg

--- application/views/page/Login.php
+++ application/views/page/Login.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../asset/css/style.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css') ?>">
     <title>Login</title>
 </head>
 <body>
+
     <div class="container">
     <p class="login-text" style="font-size: 2rem; font-weight: 800;">Login</p>
         <form action="login.php" method="get" class="login-email" >
--- index.php
+++ index.php
@@ -312,5 +312,6 @@ switch (ENVIRONMENT)
  *
  * And away we go...
  */
+
 require_once BASEPATH.'core/CodeIgniter.php';
 ?>
